e97efb02f971a4f802335c953c57b8ae5998101c
Shagun-29 <shagun.sharma@venturepact.com>
2019-01-25 16:02:37 +0530

    --adding toggle--

--- tinymce-custom-class.js
+++ tinymce-custom-class.js
@@ -37,7 +37,6 @@ editorShortcode="[outgrow][/outgrow]";
           
       });
 
-    
       editor.addCommand('custom_class', function() {
         var win = editor.windowManager.open({
               title:'Outgrow API',
@@ -55,18 +54,18 @@ editorShortcode="[outgrow][/outgrow]";
                     editor.insertContent(editorShortcode);
                   },
               }
-          });
+        });
       //     document.getElementById("tiny-mce-custom-og-body").innerHTML="<div id='getAPI'><div id='selectAPI'><form method='post'><select name='inptProduct' Placeholder='Enter API KEY' id='select-custom-api' onclick='selectAPI()'>"
       //  +"</select></form></div></div>";
       
       document.getElementById("tiny-mce-custom-og-body").innerHTML="<div id='getAPI'>"+
       "<div class='div-input-field'><input type='text' class='input-field-text' onclick='showSelectOptions()' placeholder='ENTER API KEY' ></div>"+
-      "<div id='selectAPI' class='hide'><form method='post'><select name='inptProduct' Placeholder='Enter API KEY' id='select-custom-api' onclick='selectAPI()'></select></form></div>"+
+      "<div id='selectAPI' class='hide'><form method='post'><select name='inputProduct' Placeholder='Enter API KEY' id='select-custom-api' onclick='selectAPI()'></select></form></div>"+
       "</div>";
 
 
     if(apiSet.length>0){
- apiSet.forEach(element => {
+    apiSet.forEach(element => {
       document.getElementById("select-custom-api").insertAdjacentHTML('afterbegin',"<option>"+element+"</option>")
     });
     }
@@ -79,19 +78,21 @@ editorShortcode="[outgrow][/outgrow]";
 
 
 function selectAPI(){
+  // var data="";
+  // document.getElementById("api-card").innerHTML=data;
   api=document.getElementById('select-custom-api').value;
   $.ajax({
     url:'https://api-calc.outgrow.co/api/v1/calculator?status=Live&type=All&sort=alpha_as',
     headers: {'API-KEY': api},
-     type: 'get',
-   success: function(response){
-   var refinedResponse=response.data;
-  document.getElementById("tiny-mce-custom-og-body").insertAdjacentHTML("beforeend","<div id='api-card'>");
+    type: 'get',
+    success: function(response){
+    var refinedResponse=response.data;
+   document.getElementById("tiny-mce-custom-og-body").insertAdjacentHTML("beforeend","<div id='api-card'>");
 
   if(response.success==true){
     refinedResponse.forEach(element => {
-      document.getElementById("api-card").insertAdjacentHTML(`afterend`,`<div class="shortcodecard-row" id="shortcodecard-row-id"><div class="shortcodecard-col">
-      <div class="shortcodecard-body">
+      document.getElementById("api-card").insertAdjacentHTML(`afterend`,`<div class="shortcodecard-row" id="shortcodecard-row-id"><div id="shortcodecard-col">
+      <div id="shortcode-card-body">
        <div class="shortcodecard-content" id="${element.id}" onclick="viewDetails('${element.id}','${element.calc_url}','${element.short_url}')">${element.meta_data.title}</div>
        </div>
       </div></div>`);
@@ -110,16 +111,21 @@ function selectAPI(){
 }
 
 function viewDetails(id,url,s_url){
+
   console.log("::id::",id,"::url::",url,"::surl::",s_url);
-  document.getElementById(id).innerHTML=`<div id="shortcode-header"></div>`+
-      `<div id="main-div-section"><div id="section-div-1">`+
-      `<div id="embed1" class="embed" onclick="getEmbedCode('embed1')">EMBED + MOBILE FULL SCREEN</div>`+
-      `<div id="embed2" class="embed" onclick="getEmbedCode('embed2')">EMBED + MOBILE IN PAGE</div>`+
-      `<div id="embed3" class="embed" onclick="getEmbedCode('embed3')">POP UP</div>`+
-      `<div id="embed4" class="embed" onclick="getEmbedCode('embed4')">CHAT</div>`+
-      `<div id="embed5" class="embed" onclick="getEmbedCode('embed5')">CUSTOM EMBED</div>`+
+  document.getElementById(id).innerHTML=`<div id="shortcode-header">`+
+    `<div id="main-div-section">`+
+      `<div id="section-div-1">`+
+          `<div id="embed1" class="embed" onclick="getEmbedCode('embed1')">EMBED + MOBILE FULL SCREEN</div>`+
+          `<div id="embed2" class="embed" onclick="getEmbedCode('embed2')">EMBED + MOBILE IN PAGE</div>`+
+          `<div id="embed3" class="embed" onclick="getEmbedCode('embed3')">POP UP</div>`+
+          `<div id="embed4" class="embed" onclick="getEmbedCode('embed4')">CHAT</div>`+
+          `<div id="embed5" class="embed" onclick="getEmbedCode('embed5')">CUSTOM EMBED</div>`+
       `</div>`+
-      `<div id="section-div-2"><div id="shortcode-text">COPY HERE</div></div></div>`;
+      `<div id="section-div-2">`+
+        `<div id="shortcode-text">COPY HERE</div>`+
+      `</div>`+
+    `</div></div>`;
 
       // editor.addButton('myblockquotebtn', {
       //   title: 'My Blockquote',
